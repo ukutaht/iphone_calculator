ab7273b45f373f7ec2bf2b32959e15a933ac8ba2
heruku <uku.taht@gmail.com>
2013-12-20 14:44:45 +0200

    adding, subtracting, dividing, multiplying done

--- Calculator/CalcViewController.h
+++ Calculator/CalcViewController.h
@@ -8,6 +8,31 @@
 
 #import <UIKit/UIKit.h>
 
+int Method;
+int Select;
+float RunningTotal;
+
 @interface CalcViewController : UIViewController
+{
+    IBOutlet UILabel *Screen;
+}
+
+-(IBAction)Number1:(id)sender;
+-(IBAction)Number2:(id)sender;
+-(IBAction)Number3:(id)sender;
+-(IBAction)Number4:(id)sender;
+-(IBAction)Number5:(id)sender;
+-(IBAction)Number6:(id)sender;
+-(IBAction)Number7:(id)sender;
+-(IBAction)Number8:(id)sender;
+-(IBAction)Number9:(id)sender;
+-(IBAction)Number0:(id)sender;
+
+-(IBAction)Times:(id)sender;
+-(IBAction)Divide:(id)sender;
+-(IBAction)Subract:(id)sender;
+-(IBAction)Add:(id)sender;
+-(IBAction)Equals:(id)sender;
+-(IBAction)Clear:(id)sender;
 
 @end
--- Calculator/CalcViewController.m
+++ Calculator/CalcViewController.m
@@ -14,6 +14,249 @@
 
 @implementation CalcViewController
 
+-(IBAction)Number1:(id)sender{
+    
+    Select = Select * 10;
+    Select = Select + 1;
+    
+    Screen.text = [NSString stringWithFormat:@"%i", Select];
+    
+}
+
+-(IBAction)Number2:(id)sender{
+    
+    Select = Select * 10;
+    Select = Select + 2;
+    
+    Screen.text = [NSString stringWithFormat:@"%i", Select];
+    
+}
+
+-(IBAction)Number3:(id)sender{
+    
+    Select = Select * 10;
+    Select = Select + 3;
+    
+    Screen.text = [NSString stringWithFormat:@"%i", Select];
+    
+}
+
+-(IBAction)Number4:(id)sender{
+    
+    Select = Select * 10;
+    Select = Select + 4;
+    
+    Screen.text = [NSString stringWithFormat:@"%i", Select];
+    
+}
+
+-(IBAction)Number5:(id)sender{
+    
+    Select = Select * 10;
+    Select = Select + 5;
+    
+    Screen.text = [NSString stringWithFormat:@"%i", Select];
+    
+}
+
+-(IBAction)Number6:(id)sender{
+    
+    Select = Select * 10;
+    Select = Select + 6;
+    
+    Screen.text = [NSString stringWithFormat:@"%i", Select];
+    
+}
+
+-(IBAction)Number7:(id)sender{
+    
+    Select = Select * 10;
+    Select = Select + 7;
+    
+    Screen.text = [NSString stringWithFormat:@"%i", Select];
+    
+}
+
+-(IBAction)Number8:(id)sender{
+    
+    Select = Select * 10;
+    Select = Select + 8;
+    
+    Screen.text = [NSString stringWithFormat:@"%i", Select];
+    
+}
+
+-(IBAction)Number9:(id)sender{
+    
+    Select = Select * 10;
+    Select = Select + 9;
+    
+    Screen.text = [NSString stringWithFormat:@"%i", Select];
+    
+}
+
+-(IBAction)Number0:(id)sender{
+    
+    Select = Select * 10;
+    
+    Screen.text = [NSString stringWithFormat:@"%i", Select];
+    
+}
+
+-(IBAction)Times:(id)sender{
+    
+    if(RunningTotal == 0){
+        RunningTotal = Select;
+    } else {
+        switch (Method) {
+            case 1:
+                RunningTotal = RunningTotal * Select;
+                break;
+            case 2:
+                RunningTotal = RunningTotal / Select;
+                break;
+            case 3:
+                RunningTotal = RunningTotal - Select;
+                break;
+            case 4:
+                RunningTotal = RunningTotal + Select;
+                break;
+            default:
+                break;
+        }
+    }
+    
+    Select = 0;
+    Method = 1;
+    
+}
+
+-(IBAction)Divide:(id)sender{
+    
+    if(RunningTotal == 0){
+        RunningTotal = Select;
+    } else {
+        switch (Method) {
+            case 1:
+                RunningTotal = RunningTotal * Select;
+                break;
+            case 2:
+                RunningTotal = RunningTotal / Select;
+                break;
+            case 3:
+                RunningTotal = RunningTotal - Select;
+                break;
+            case 4:
+                RunningTotal = RunningTotal + Select;
+                break;
+            default:
+                break;
+        }
+    }
+    
+    Select = 0;
+    Method = 2;
+    
+    
+}
+
+-(IBAction)Subract:(id)sender{
+    
+    if(RunningTotal == 0){
+        RunningTotal = Select;
+    } else {
+        switch (Method) {
+            case 1:
+                RunningTotal = RunningTotal * Select;
+                break;
+            case 2:
+                RunningTotal = RunningTotal / Select;
+                break;
+            case 3:
+                RunningTotal = RunningTotal - Select;
+                break;
+            case 4:
+                RunningTotal = RunningTotal + Select;
+                break;
+            default:
+                break;
+        }
+    }
+    
+    Select = 0;
+    Method = 3;
+    
+}
+
+-(IBAction)Add:(id)sender{
+    
+    if(RunningTotal == 0){
+        RunningTotal = Select;
+    } else {
+        switch (Method) {
+            case 1:
+                RunningTotal = RunningTotal * Select;
+                break;
+            case 2:
+                RunningTotal = RunningTotal / Select;
+                break;
+            case 3:
+                RunningTotal = RunningTotal - Select;
+                break;
+            case 4:
+                RunningTotal = RunningTotal + Select;
+                break;
+            default:
+                break;
+        }
+    }
+    
+    Select = 0;
+    Method = 4;
+    
+}
+
+-(IBAction)Equals:(id)sender{
+    
+    if(RunningTotal == 0){
+        RunningTotal = Select;
+    } else {
+        switch (Method) {
+            case 1:
+                RunningTotal = RunningTotal * Select;
+                break;
+            case 2:
+                RunningTotal = RunningTotal / Select;
+                break;
+            case 3:
+                RunningTotal = RunningTotal - Select;
+                break;
+            case 4:
+                RunningTotal = RunningTotal + Select;
+                break;
+            default:
+                break;
+        }
+    }
+    
+    Select = 0;
+    Method = 0;
+    
+    Screen.text = [NSString stringWithFormat: @"%.2f", RunningTotal];
+    
+    
+}
+
+-(IBAction)Clear:(id)sender{
+    
+    Method = 0;
+    Select = 0;
+    RunningTotal = 0.0;
+    
+    Screen.text = [NSString stringWithFormat: @"0"];
+    
+}
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
